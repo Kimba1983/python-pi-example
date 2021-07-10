85333c467dc84e2dcbf04c01943652a0f46eb84d
Kimba1983 <augustomiguel97@outlook.com>
2021-07-10 12:16:34 -0300

    Change amde

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def main(msg):
+    //Another comment.
     //Print a message.
     //Adding a different comment.
     print("msg)"Print a message
  print("msg)"
 
 main("Hello People!")
+
+//change made
